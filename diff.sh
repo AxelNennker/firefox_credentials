8ccaeda1dcd9e0d9c2110cf713e0e09d2c16611a
Axel Nennker <axel@nennker.de>
2015-10-02 20:14:22 +0200

    ???

--- diff.sh
+++ diff.sh
@@ -5,3 +5,7 @@ diff CredentialContainer.webidl ../firefox/dom/webidl/CredentialContainer.webidl
 (cd ../firefox && hg diff -U8 dom/base/Navigator.h > /tmp/Navigator.h.patch && diff /tmp/Navigator.h.patch ../W3Credentials/Navigator.h.patch)
 (cd ../firefox && hg diff -U8 dom/base/Navigator.cpp > /tmp/Navigator.cpp.patch && diff /tmp/Navigator.cpp.patch ../W3Credentials/Navigator.cpp.patch)
 #(cd ../firefox && hg diff -U8 dom/webidl/Navigator.webidl > /tmp/Navigator.webidl.patch && diff /tmp/Navigator.webidl.patch ../W3Credentials/Navigator.webidl.patch)
+(cd ../firefox && hg diff -U8 dom/ipc/ContentParent.cpp > /tmp/ContentParent.cpp-patch && diff /tmp/ContentParent.cpp-patch ../W3Credentials/ContentParent.cpp-patch)
+(cd ../firefox && hg diff -U8 dom/ipc/ContentParent.h > /tmp/ContentParent.h-patch && diff /tmp/ContentParent.h-patch ../W3Credentials/ContentParent.h-patch)
+(cd ../firefox && hg diff -U8 dom/ipc/PContent.ipdl > /tmp/PContentipdl-patch && diff /tmp/PContentipdl-patch ../W3Credentials/PContent.ipdl-patch)
+(cd ../firefox && hg diff -U8 dom/bindings/Bindings.conf > /tmp/Bindings.conf.patch && diff /tmp/Bindings.conf.patch ../W3Credentials/Bindings.conf.patch)
